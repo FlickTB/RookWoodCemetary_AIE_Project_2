b8130184576cebd9ce5de4e04cb82367cfe07f48
DT5642 <165235056+DT5642@users.noreply.github.com>
2024-08-27 11:07:50 +1000

    Prefabs
    Created prefabs of the player and the camera setup to be used in other scenes of the project. Additionally the load scene script is now set up to load "Jack Scene"

--- Assets/Scripts/LoadScene.cs
+++ Assets/Scripts/LoadScene.cs
@@ -7,6 +7,6 @@ public class LoadScene : MonoBehaviour
 {
     public void load()                                                                                                      //Function for the scene loading
     {
-        SceneManager.LoadScene("Main Scene");                                                                               //Loads the main game scene
+        SceneManager.LoadScene("Jack Scene");                                                                               //Loads the main game scene
     }
 }
