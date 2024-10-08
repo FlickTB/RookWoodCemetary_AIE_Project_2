1dc0596ac86d041dd9aa0b4873d714eef34ba5ae
DT5642 <165235056+DT5642@users.noreply.github.com>
2024-10-08 16:58:09 +1100

    Code Comments
    Commented Dash Code

--- Assets/Scripts/Legacy Scripts/Pickup.cs
+++ Assets/Scripts/Legacy Scripts/Pickup.cs
@@ -4,30 +4,30 @@ using UnityEngine;
 
 public class Pickup : MonoBehaviour
 {
-    [SerializeField] bool inRange; // Checks if the player is close to the object, can be seen in Unity
+    [SerializeField] bool inRange;                                                                                          // Checks if the player is close to the object, can be seen in Unity
 
-    void Update() // Runs every frame
+    void Update()                                                                                                           // Runs every frame
     {
-        if (inRange == true && Input.GetKeyDown("e")) // Checks that they player is in range and presses the input
+        if (inRange == true && Input.GetKeyDown("e"))                                                                       // Checks that they player is in range and presses the input
         {
-            Debug.Log("Picked up"); // Confirms in Unity that the script ran correctly
-            Destroy(gameObject); // Removes the object from the game world so it doesn't duplicate
+            Debug.Log("Picked up");                                                                                         // Confirms in Unity that the script ran correctly
+            Destroy(gameObject);                                                                                            // Removes the object from the game world so it doesn't duplicate
         }
     }
 
-    void OnTriggerEnter(Collider other) // The check when the player is close
+    void OnTriggerEnter(Collider other)                                                                                     // The check when the player is close
     {
-        if (other.CompareTag("Player")) //Checks if it is the player
+        if (other.CompareTag("Player"))                                                                                     //Checks if it is the player
         {
-            inRange = true; // Makes its so the player can pick up the object
+            inRange = true;                                                                                                 // Makes its so the player can pick up the object
         }
     }
 
-    void OnTriggerExit(Collider other) // The check when the player is too far
+    void OnTriggerExit(Collider other)                                                                                      // The check when the player is too far
     {
-        if (other.CompareTag("Player")) // Checks if it is the player
+        if (other.CompareTag("Player"))                                                                                     // Checks if it is the player
         {
-            inRange = false; // Makes its so the player can't pick up the object
+            inRange = false;                                                                                                // Makes its so the player can't pick up the object
         }
     }
 }
