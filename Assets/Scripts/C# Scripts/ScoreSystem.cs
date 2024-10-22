a95c981ba5c396d02a24869392fe9e267695b51e
DT5642 <165235056+DT5642@users.noreply.github.com>
2024-10-22 16:14:51 +1100

    Fixed the Timer Bug
    The issue regarding the timer points being reapplied after completing the level has been resolved

--- Assets/Scripts/C# Scripts/ScoreSystem.cs
+++ Assets/Scripts/C# Scripts/ScoreSystem.cs
@@ -12,11 +12,13 @@ public class ScoreSystem : MonoBehaviour
     public float timeLeft;
     public float timeMultiplier;
     public bool levelComplete;
+    public bool reachedEnd;
 
     void OnTriggerEnter(Collider other)
     {
         levelComplete = true;
         Score();
+        reachedEnd = true;
     }
 
     void Update()
@@ -37,6 +39,7 @@ public class ScoreSystem : MonoBehaviour
     void Start()
     {
         levelComplete = false;
+        reachedEnd = false;
     }
 
     void Timer(float currentTime)
@@ -55,10 +58,15 @@ public class ScoreSystem : MonoBehaviour
         {
             scoreText.text = string.Format("Score: {0000:0}", finalscore);
         }
-        else if (levelComplete == true)
+        else if (levelComplete == true && reachedEnd == false)
         {
             finalscore += (timeLeft * timeMultiplier);
             scoreText.text = string.Format("Score: {0000:0}", finalscore);
         }
     }
+
+    //void End()
+    //{
+    //    reachedEnd = true;
+    //}
 }
