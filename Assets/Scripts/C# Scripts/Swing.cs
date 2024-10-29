e48a61e5cf598ac474284360e5b7f7b149d614b5
DT5642 <165235056+DT5642@users.noreply.github.com>
2024-10-29 12:16:27 +1100

    Added Swing Return
    After the sing animation ends, it will play an animation of the shovel returning, rather than it teleporting back into position

--- Assets/Scripts/C# Scripts/Swing.cs
+++ Assets/Scripts/C# Scripts/Swing.cs
@@ -5,21 +5,34 @@ using UnityEngine;
 public class Swing : MonoBehaviour
 {
     [SerializeField] private Animator animator;                                                                             //Refernce for the animation
+    private bool canSecondSwing = false;
 
     void Update()                                                                                                           //Function called every frame
     {
         if (Input.GetKeyDown(KeyCode.Mouse0))                                                                               //If the player left clicks
         {
-            animator.SetBool("Swing", true);                                                                                //Allows the animation to play
+            animator.SetBool("Swing 1", true);                                                                              //Allows the animation to play
             GetComponent<BoxCollider>().enabled = true;                                                                     //Activates the shovel hitbox
+            canSecondSwing = true;
 
-            Invoke(nameof(Cooldown), 1f);                                                                                   //Calls on the other function
+            if (Input.GetKey(KeyCode.Mouse1) && canSecondSwing)                                                         //If the player wants to do a combo
+            {
+                animator.SetBool("Swing 2", true);                                                                          //Allows second animation to play
+                GetComponent<BoxCollider>().enabled = true;                                                                 //Activates the shovel hit box
+                canSecondSwing = false;
+                Invoke(nameof(Cooldown), .75f);                                                                               //Calls on the other function
+            }
+            else
+            {
+                Invoke(nameof(Cooldown), .75f);                                                                               //Calls on the other function
+            }
         }
     }
 
     void Cooldown()                                                                                                         //Function for stopping the swing
     {
-        animator.SetBool("Swing", false);                                                                                   //Stops the swing animation
+        animator.SetBool("Swing 1", false);                                                                                 //Stops the swing animation
+        animator.SetBool("Swing 2", false);                                                                                 //Stops the swing animation
         GetComponent<BoxCollider>().enabled = false;                                                                        //Disables the hitbox
     }
 }
